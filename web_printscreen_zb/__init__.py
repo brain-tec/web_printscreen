adafe3dd6d39e1e170a5b846f92b14f10a41dd01
dufresnedavid <david.dufresne@savoirfairelinux.com>
2015-07-15 01:48:09 -0400

    Fix PEP8, Modify header, Add footer
    The header will contain the company's logo
    The footer will contain the date and page number

--- web_printscreen_zb/__init__.py
+++ web_printscreen_zb/__init__.py
@@ -4,6 +4,7 @@
 #    Copyright (c) 2013 ZestyBeanz Technologies Pvt. Ltd.
 #    (http://wwww.zbeanztech.com)
 #    contact@zbeanztech.com
+#    Copyright (c) 2015 Savoir-faire Linux
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -20,7 +21,5 @@
 #
 ##############################################################################
 
-import trml2pdf
-import controllers
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import trml2pdf
+from . import controllers
